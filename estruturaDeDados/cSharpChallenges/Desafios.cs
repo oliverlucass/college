18035e3e64b6c5ca92fc28f7c16ccbd82d0e8313
oliverlucass <olucasoliveira.contato@gmail.com>
2022-03-28 11:58:20 -0300

    class

---
+++ estruturaDeDados/cSharpChallenges/Desafios.cs
@@ -0,0 +1,98 @@
+﻿using System;
+
+namespace Desafios
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            // string n;
+
+            // Console.WriteLine("Insira um número:\n");
+
+            // n = Console.ReadLine();
+            // Console.WriteLine("\nNúmero Inserido foi: " + n);
+
+
+
+
+            // int number1, number2, result;
+            // Console.WriteLine("Insira dois números para serem somados!!");
+            // Console.WriteLine("Insira o primeiro número:");
+            // number1 = int.Parse(Console.ReadLine());
+            // Console.WriteLine("Insira o segundo número:");
+            // number2 = int.Parse(Console.ReadLine());
+            // result = number1 + number2;
+            // Console.WriteLine("A soma dos números inseridos é: " + result);
+
+
+
+
+            // int n1, n2;
+            // Console.WriteLine("n1:");
+            // n1 = int.Parse(Console.ReadLine());
+            // Console.WriteLine("n2:");
+            // n2 = int.Parse(Console.ReadLine());
+
+            // if (n1 > n2)
+            // {
+            //     Console.WriteLine("maior: " + n1);
+            // }
+            // else
+            // {
+            //     Console.WriteLine("maior:" + n2);
+            // }
+
+
+
+
+            // int n;
+            // Console.WriteLine("n:");
+            // n = int.Parse(Console.ReadLine());
+
+            // if (n % 2 == 0)
+            // {
+            //     Console.WriteLine("Par");
+            // }
+            // else
+            // {
+            //     Console.WriteLine("Impar");
+            // }
+
+
+
+
+            // int anterior = 0;
+            // int maior = 0;
+
+            // Console.WriteLine("Insira cinco números:");
+
+            // for (int i = 1; i <= 5; i++)
+            // {
+            //     anterior = int.Parse(Console.ReadLine());
+            //     if (anterior > maior)
+            //     {
+            //         maior = anterior;
+            //     }
+            // }
+
+            // Console.WriteLine("maior:" + maior);
+
+
+
+
+            // int n = 0;
+
+            // Console.WriteLine("Isira um número para ver tabuada:");
+            // n = int.Parse(Console.ReadLine());
+            // Console.WriteLine('\n');
+
+            // for (int i = 1; i <= 10; i++)
+            // {
+            //     Console.WriteLine(n * i);
+            // }
+
+        }
+    }
+}
