4f6e16cd6021d735c04dc7e6bdf6b8db02861481
oliverlucass <olucasoliveira.contato@gmail.com>
2022-03-28 11:41:43 -0300

    :rocket:

--- estruturaDeDados/baseDeDadosNET6/BaseDeDados/Program.cs
+++
@@ -1,7 +0,0 @@
-﻿using BaseDeDados.UI;
-using BaseDeDados.Actions;
-
-var mainOperation = new UserAction();
-
-MainInterface.MostrarApresentacao();
-mainOperation.Begin();
--- estruturaDeDados/baseDeDadosNET6/BaseDeDados/src/Usuario/Usuario.cs
+++
@@ -1,9 +0,0 @@
-namespace BaseDeDados.Usuario;
-
-public class Usuario {
-    public int RA { get; set; }
-    public string Nome { get; set; } = "";
-    public int CPF { get; set; }
-    public string? Email { get; set; }
-    public DateTime DataDeNascimento { get; set; }
-}
